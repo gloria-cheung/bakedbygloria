c98188a056014f8220f6297cda0892c2c39025ab
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-09-06 14:48:10 -0400

    create article to display covid19 message

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,25 @@
+import "./Article.scss";
+
+export default function Article() {
+  return (
+    <div className="container">
+      <div className="row p-5">
+        <div className="col p-5"></div>
+        <div className="col-7 d-flex flex-column justify-content-center align-items-center ">
+          <p>
+            _____________________________________________________________________
+          </p>
+          <p>
+            To ensure safety during COVID-19, Face Masks are mandatory upon pick
+            up.
+          </p>
+          <p>Contactless Delivery available upon request.</p>
+          <p>
+            _____________________________________________________________________
+          </p>
+        </div>
+        <div className="col"></div>
+      </div>
+    </div>
+  );
+}
