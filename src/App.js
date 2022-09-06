33244033a959df5b9cbdf6d2b00782ffc90a6e88
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-09-06 18:32:45 -0400

    remove nav and footer from app component and move to index

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import "./App.scss";
-import Nav from "./components/nav/Nav";
 import Banner from "./components/banner/Banner";
 import Article from "./components/article/Article";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
-import Pricing from "./components/pricing/Pricing";
 
 function App() {
   return (
     <div className="App">
       <div className="justify-content-md-center">
-        <Nav />
         <Banner />
         <Article />
         <About />
         <Contact />
-        <div id="footer"></div>
       </div>
     </div>
   );
