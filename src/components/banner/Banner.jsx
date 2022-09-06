3714b26420d01d365be4d0511fa9c92f400c2828
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-09-06 14:48:33 -0400

    create banner with images

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,22 @@
+import "./Banner.scss";
+
+export default function Banner() {
+  return (
+    <section>
+      <div className="row d-flex justify-content-center g-0">
+        <div className="col-3">
+          <img src="/theme_cakes/41.JPG" alt="crowncake" />
+        </div>
+        <div className="col-3">
+          <img src="/twotier_cakes/8.JPG" alt="nakedcake" />
+        </div>
+        <div className="col-3">
+          <img src="/theme_cakes/66.JPG" alt="eyorecake" />
+        </div>
+        <div className="col-3">
+          <img src="/twotier_cakes/18.JPG" alt="dripcake" />
+        </div>
+      </div>
+    </section>
+  );
+}
