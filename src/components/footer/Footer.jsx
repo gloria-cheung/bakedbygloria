f6bc6172c98546452ebfc75a024cacb81a524ffe
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-09-06 18:34:58 -0400

    create simple footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.scss";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <footer class="py-5 bg-light">
+        <div class="container">
+          <p class="m-0 text-center">Copyright &copy; BakedByGloria 2022</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
