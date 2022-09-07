4697e578898105db04d5b36c11cecaad0fd34a64
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-09-06 22:17:11 -0400

    add data for cakes

---
+++ src/data.js
@@ -0,0 +1,433 @@
+export const themedCakeData = [
+  {
+    id: 1,
+    title: "Basketball Cake",
+  },
+  {
+    id: 2,
+    title: "Pingpong Cake",
+  },
+  {
+    id: 3,
+    title: "Paw Patrol Pink Cake",
+  },
+  {
+    id: 4,
+    title: "Airplane Cake",
+  },
+  {
+    id: 5,
+    title: "Elmo Cake",
+  },
+  {
+    id: 6,
+    title: "Chessboard Cake",
+  },
+  {
+    id: 7,
+    title: "Birthday Cake",
+  },
+  {
+    id: 8,
+    title: "70th Birthday Cake",
+  },
+  {
+    id: 9,
+    title: "Baby Shark Cake",
+  },
+  {
+    id: 10,
+    title: "Pharmacy Cake",
+  },
+  {
+    id: 11,
+    title: "Minion Cake",
+  },
+  {
+    id: 12,
+    title: "Beehive Cake",
+  },
+  {
+    id: 13,
+    title: "Basketball Player Cake",
+  },
+  {
+    id: 14,
+    title: "Elmo Two Tier Cake",
+  },
+  {
+    id: 15,
+    title: "Minnie Mouse Cake",
+  },
+  {
+    id: 16,
+    title: "Minnie Mouse Cake",
+  },
+  {
+    id: 17,
+    title: "Minnie Mouse Cake",
+  },
+  {
+    id: 18,
+    title: "Beer Cake",
+  },
+  {
+    id: 19,
+    title: "Genie Cake",
+  },
+  {
+    id: 20,
+    title: "Animal Cake",
+  },
+  {
+    id: 21,
+    title: "Thomas Train Cake",
+  },
+  {
+    id: 22,
+    title: "Roblox Cake",
+  },
+  {
+    id: 23,
+    title: "Corgi Cake",
+  },
+  {
+    id: 24,
+    title: "Travel Cake",
+  },
+  {
+    id: 25,
+    title: "Gucci Cake",
+  },
+  {
+    id: 26,
+    title: "Animal Cake",
+  },
+  {
+    id: 27,
+    title: "Dinosaur Cake",
+  },
+  {
+    id: 28,
+    title: "Playstation Cake",
+  },
+  {
+    id: 29,
+    title: "Basketball Cake",
+  },
+  {
+    id: 30,
+    title: "Bee Cake",
+  },
+  {
+    id: 31,
+    title: "Flash Cake",
+  },
+  {
+    id: 32,
+    title: "Beer Cake",
+  },
+  {
+    id: 33,
+    title: "Tuxedo Cake",
+  },
+  {
+    id: 34,
+    title: "Alcohol Cake",
+  },
+  {
+    id: 35,
+    title: "Spiderman Cake",
+  },
+  {
+    id: 36,
+    title: "Mustang Cake",
+  },
+  {
+    id: 37,
+    title: "Hello Kitty Cake",
+  },
+  {
+    id: 38,
+    title: "Minecraft Cake",
+  },
+  {
+    id: 39,
+    title: "Tiffany Cake",
+  },
+  {
+    id: 40,
+    title: "Graduation Cake",
+  },
+  {
+    id: 41,
+    title: "Crown Cake",
+  },
+  {
+    id: 42,
+    title: "Seaworld Cake",
+  },
+  {
+    id: 43,
+    title: "Cute Animal Cake",
+  },
+  {
+    id: 44,
+    title: "Starwars Cake",
+  },
+  {
+    id: 45,
+    title: "Minion Cake",
+  },
+  {
+    id: 46,
+    title: "Custom Dog Cake",
+  },
+  {
+    id: 47,
+    title: "Popeyes Cake",
+  },
+  {
+    id: 48,
+    title: "Power Ranger Cake",
+  },
+  {
+    id: 49,
+    title: "Photography Cake",
+  },
+  {
+    id: 50,
+    title: "Transformer Cake",
+  },
+  {
+    id: 51,
+    title: "Racecar Cake",
+  },
+  {
+    id: 52,
+    title: "Unicorn Cake",
+  },
+  {
+    id: 53,
+    title: "Hello Kitty Cake",
+  },
+  {
+    id: 54,
+    title: "Avengers Cake",
+  },
+  {
+    id: 55,
+    title: "Raptors Cake",
+  },
+  {
+    id: 56,
+    title: "Bowling Cake",
+  },
+  {
+    id: 57,
+    title: "Captain America Cake",
+  },
+  {
+    id: 58,
+    title: "Batman Cake",
+  },
+  {
+    id: 59,
+    title: "Raptors Cake",
+  },
+  {
+    id: 60,
+    title: "Chanel Cake",
+  },
+  {
+    id: 61,
+    title: "Animal Cake",
+  },
+  {
+    id: 62,
+    title: "Playstation Cake",
+  },
+  {
+    id: 63,
+    title: "Poker Cake",
+  },
+  {
+    id: 64,
+    title: "Toystory Cake",
+  },
+  {
+    id: 65,
+    title: "Roblox Cake",
+  },
+  {
+    id: 66,
+    title: "Eeyore Cake",
+  },
+  {
+    id: 67,
+    title: "Bunny Cake",
+  },
+  {
+    id: 68,
+    title: "Bee Cake",
+  },
+  {
+    id: 69,
+    title: "Flamingo Cake",
+  },
+  {
+    id: 70,
+    title: "Baby Shark Cake",
+  },
+  {
+    id: 71,
+    title: "Tiffany Cake",
+  },
+  {
+    id: 72,
+    title: "Llama Cake",
+  },
+  {
+    id: 73,
+    title: "Doctor Cake",
+  },
+  {
+    id: 74,
+    title: "Graduation Cake",
+  },
+  {
+    id: 75,
+    title: "Construction Cake",
+  },
+  {
+    id: 76,
+    title: "Graduation Cake",
+  },
+  {
+    id: 77,
+    title: "Pokeball Cake",
+  },
+  {
+    id: 78,
+    title: "Paw Patrol Cake",
+  },
+  {
+    id: 79,
+    title: "Pig Cake",
+  },
+  {
+    id: 80,
+    title: "Monkey Cake",
+  },
+  {
+    id: 81,
+    title: "Lego Cake",
+  },
+  {
+    id: 82,
+    title: "Hello Kitty Cake",
+  },
+  {
+    id: 83,
+    title: "Hello Kitty Cake",
+  },
+  {
+    id: 84,
+    title: "Chiropractor Cake",
+  },
+  {
+    id: 85,
+    title: "Construction Cake",
+  },
+  {
+    id: 86,
+    title: "Chinese Zodiac Cake",
+  },
+];
+
+export const twoTierCakeData = [
+  {
+    id: 1,
+    title: "First Birthday Cake",
+  },
+  {
+    id: 2,
+    title: "Blue Cake",
+  },
+  {
+    id: 3,
+    title: "Semi Naked Cake",
+  },
+  {
+    id: 4,
+    title: "Pink Flower Cake",
+  },
+  {
+    id: 5,
+    title: "First Birthday Cake",
+  },
+  {
+    id: 6,
+    title: "Graduation Cake",
+  },
+  {
+    id: 7,
+    title: "Wedding Cake",
+  },
+  {
+    id: 8,
+    title: "Semi Naked Cake",
+  },
+  {
+    id: 9,
+    title: "Sesame Street Cake",
+  },
+  {
+    id: 10,
+    title: "Leaf Cake",
+  },
+  {
+    id: 11,
+    title: "Pink Cake",
+  },
+  {
+    id: 12,
+    title: "Mickey Mouse Cake",
+  },
+  {
+    id: 13,
+    title: "Marvel Cake",
+  },
+  {
+    id: 14,
+    title: "Pikachu Cake",
+  },
+  {
+    id: 15,
+    title: "Avengers Cake",
+  },
+  {
+    id: 16,
+    title: "50th Birthday Cake",
+  },
+  {
+    id: 17,
+    title: "18th Birthday Cake",
+  },
+  {
+    id: 18,
+    title: "Macaron Drip Cake",
+  },
+  {
+    id: 19,
+    title: "Baptism Cake",
+  },
+  {
+    id: 20,
+    title: "Red Velvet Cake",
+  },
+  {
+    id: 21,
+    title: "Wedding Cake",
+  },
+];
