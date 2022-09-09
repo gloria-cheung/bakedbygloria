87531ae1fb0bc5564c7cdfbaf7186fe297d9550d
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-09-08 22:52:01 -0400

    fix to allow for collapsable nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,13 +4,13 @@ export default function Nav() {
   return (
     <nav class="navbar navbar-expand-lg navbar border-bottom sticky-top nav-bg">
       <a class="navbar-brand" href="/">
-        <img src="bakedbygloria.png" alt="logo" className="w-75" />
+        <img src="bakedbygloria.png" alt="logo" className="w-75 me-5" />
       </a>
       <button
-        class="navbar-toggler"
+        class="navbar-toggler me-4"
         type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
         aria-controls="navbarNav"
         aria-expanded="false"
         aria-label="Toggle navigation"
@@ -18,30 +18,23 @@ export default function Nav() {
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
-        <div className="w-100 d-flex justify-content-around">
-          <ul class="navbar-nav">
-            <li class="nav-item p-4">
-              <a class="nav-link" href="/cakes">
-                CAKES
-              </a>
-            </li>
-            <li class="nav-item p-4">
-              <a class="nav-link" href="/pricing">
-                PRICING
-              </a>
-            </li>
-            <li class="nav-item p-4">
-              <a class="nav-link" href="/contact">
-                CONTACT US
-              </a>
-            </li>
-            <li class="nav-item p-4">
-              <a class="nav-link" href="/about">
-                ABOUT
-              </a>
-            </li>
-          </ul>
-        </div>
+        <ul class="navbar-nav w-100 justify-content-evenly ms-5">
+          <li class="nav-item">
+            <a class="nav-link p-4" href="/cakes">
+              CAKES
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link p-4" href="/pricing">
+              PRICING
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link p-4" href="/contact">
+              CONTACT US
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
